098be208b6fe893c5a747fe8b18cc8daa02d2bc9
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-12-28 02:52:21 -0500

    project from scratch

---
+++ src/main/java/com/tareas/crudtareas/CrudtareasApplication.java
@@ -0,0 +1,13 @@
+package com.tareas.crudtareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudtareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudtareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tareas/crudtareas/CrudtareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tareas.crudtareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudtareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
